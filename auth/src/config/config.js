c26ca498b7355c14335b081fc1627b844659e3b4
Arpita Rawat <arpitarawat381@gmail.com>
2026-07-13 13:37:27 +0530

    implement UserRegister API

--- auth/src/config/config.js
+++ auth/src/config/config.js
@@ -3,7 +3,8 @@ import {config as dotenvConfig} from 'dotenv';
 dotenvConfig();
 
 const _config = {
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
 
 export default _config;
--- auth/src/controllers/auth.controller.js
+++ auth/src/controllers/auth.controller.js
@@ -2,3 +2,46 @@ import userModel from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
+
+export async function registerUser(req, res) {
+  const {
+    email,
+    password,
+    fullname: { firstName, lastName },
+  } = req.body;
+
+  const isUserAlreadyExist = await userModel.findOne({ email });
+
+  if (isUserAlreadyExist) {
+    return (res, status(400).json({ message: "User already exists" }));
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new userModel({
+    email,
+    password: hashedPassword,
+    fullName: { firstName, lastName },
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    config.JWT_SECRET,
+    (expiresIn = "2d"),
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "USer registered successfully",
+    user: {
+      id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+      role: user.role,
+    },
+  });
+}
