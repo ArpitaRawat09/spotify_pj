a7c64f92b7bc57b1fb6cc6192b19e74612d01b06
Arpita Rawat <arpitarawat381@gmail.com>
2026-07-23 22:16:46 +0530

    Basic setup of Server

--- music/src/app.js
+++ music/src/app.js
@@ -1,5 +1,10 @@
-import express from 'express';
+import express from "express";
+import musicRoutes from "./routes/music.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/music", musicRoutes);
 
 export default app;
